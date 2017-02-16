0ed90a0b398c04bbb522d18644a0068559e495ef
eoedroso <eoedroso@yahoo.es>
2017-02-16 13:18:58 +0100

    Add files via upload

--- js/cordova.js
+++ js/cordova.js
@@ -6,7 +6,7 @@
  * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with the License.  copy of the License at
+ * with the License.  You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -26,8 +26,8 @@ if (window.cordova && !(window.cordova instanceof HTMLElement)) {
 }
 
 
-var channel = require('cordova/channel');
-var platform = require('cordova/platform');
+ var channel = require('cordova/channel');
+ var platform = require('cordova/platform');
 
 
 /**
